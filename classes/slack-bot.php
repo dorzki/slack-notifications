e866e92e9d3d45017628623613fc5286e6ba9e1f
Ran Bar-Zik <ran@bar-zik.com>
2015-10-17 09:52:29 +0300

    Adding DoxyGen standards

--- classes/slack-bot.php
+++ classes/slack-bot.php
@@ -1,14 +1,14 @@
 <?php
 /**
+ * Slack Bot Class.
+ *
  * @package   Slack Notifications
  * @since     1.0.0
  * @version   1.0.1
  * @author    Dor Zuberi <me@dorzki.co.il>
  * @link      https://www.dorzki.co.il
- *
- *
- * SLACK BOT CLASS
  */
+
 if ( ! class_exists( slackBot ) ) {
 
 	class slackBot {
