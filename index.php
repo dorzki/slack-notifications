e4712124adc2f3d8387717d8564ef0244d7370ff
Ran Bar-Zik <ran@bar-zik.com>
2015-10-17 09:50:02 +0300

    Change made in comments because Code Sniffer wants it. It is dumb change, but harmless

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-// Nothing to see here
+/**
+ * Silence is golden.
+ *
+ * @package   Slack Notifications
+ * @since     1.0.0
+ * @version   1.0.1
+ * @author    Dor Zuberi <me@dorzki.co.il>
+ * @link      https://www.dorzki.co.il
+ */
+
+/* Silence is golden.*/
