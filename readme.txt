f328c6cd461ee8ad24fba0d939da05e6967d19bb
dorzki <webmaster@dorzki.co.il>
2016-03-27 19:41:26 +0300

    Updated plugin version. Updated readme.txt

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: dorzki
 Tags: notifications, slack
 Requires at least: 4.3
 Tested up to: 4.4.2
-Stable tag: 1.0.5
+Stable tag: 1.0.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -56,6 +56,9 @@ Please feel free to contact me `webmaster[AT]dorzki.co.il`
 2. `Settings Screen - Part 2` Choose which notifications to get.
 
 == Changelog ==
+
+= 1.0.6 =
+* Fixed issue with image embed on WYSIWYG editor.
 
 = 1.0.5 =
 * Changed text domain slug to plugin slug.
--- slack-notifications.php
+++ slack-notifications.php
@@ -3,7 +3,7 @@
  * Plugin Name: Slack Notifications
  * Plugin URI: https://www.dorzki.co.il
  * Description: Add Slack integration to a channel and send desired notifications as a slack bot.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: dorzki
  * Author URI: https://www.dorzki.co.il
  * Text Domain: dorzki-notifications-to-slack
@@ -31,7 +31,7 @@ if ( ! defined( 'PLUGIN_ROOT_DIR' ) ) {
 }
 
 if ( ! defined( 'PLUGIN_VERSION' ) ) {
-	define( 'PLUGIN_VERSION', '1.0.5' );
+	define( 'PLUGIN_VERSION', '1.0.6' );
 }
 
 
