cad302cb14d3b2dedd413af5f56f679cedd63b16
Dor Zuberi <webmaster@dorzki.co.il>
2018-01-31 14:04:10 +0200

    Added user notifications.

--- core/notifications/user.php
+++ core/notifications/user.php
@@ -34,24 +34,101 @@ class User extends Notification_Type {
 		$this->object_label   = esc_html__( 'User', 'dorzki-notifications-to-slack' );
 		$this->object_options = [
 			'new_user'        => [
-				'label'  => esc_html__( 'User Registered', 'dorzki-notifications-to-slack' ),
-				'hooks'  => [
+				'label'    => esc_html__( 'User Registered', 'dorzki-notifications-to-slack' ),
+				'hooks'    => [
 					'user_register' => 'new_user',
 				],
 				'priority' => 10,
-				'params' => 1,
+				'params'   => 1,
 			],
 			'admin_logged_in' => [
-				'label'  => esc_html__( 'Administrator Logged In', 'dorzki-notifications-to-slack' ),
-				'hooks'  => [
+				'label'    => esc_html__( 'Administrator Logged In', 'dorzki-notifications-to-slack' ),
+				'hooks'    => [
 					'wp_login' => 'administrator_login',
 				],
 				'priority' => 10,
-				'params' => 2,
+				'params'   => 2,
 			],
 		];
 
 		parent::__construct();
+
+	}
+
+
+	/**
+	 * Post notification when a new user has registered.
+	 *
+	 * @param $user_id
+	 *
+	 * @return bool
+	 */
+	public function new_user( $user_id ) {
+
+		$user = get_user_by( 'id', $user_id );
+
+		if ( false === $user ) {
+			return false;
+		}
+
+		// Build notification
+		$message = __( ':bust_in_silhouette: A new user just registered on <%s|%s>.', 'dorzki-notifications-to-slack' );
+		$message = sprintf( $message, get_bloginfo( 'url' ), get_bloginfo( 'name' ) );
+
+		$attachments = [
+			[
+				'title' => esc_html__( 'User Name', 'dorzki-notifications-to-slack' ),
+				'value' => $user->display_name,
+				'short' => true,
+			],
+			[
+				'title' => esc_html__( 'User Email', 'dorzki-notifications-to-slack' ),
+				'value' => $user->user_email,
+				'short' => true,
+			],
+		];
+
+		return $this->slack_bot->send_message( $message, $attachments, [
+			'color' => '#2ecc71',
+		] );
+
+	}
+
+
+	/**
+	 * Post notification when an administrator login was detected.
+	 *
+	 * @param $username
+	 * @param $user
+	 *
+	 * @return bool
+	 */
+	public function administrator_login( $username, $user ) {
+
+		if ( ! in_array( 'administrator', $user->roles ) ) {
+			return false;
+		}
+
+		// Build notification
+		$message = __( ':necktie: Administrator login detected on <%s|%s>.', 'dorzki-notifications-to-slack' );
+		$message = sprintf( $message, get_bloginfo( 'url' ), get_bloginfo( 'name' ) );
+
+		$attachments = [
+			[
+				'title' => esc_html__( 'Username', 'dorzki-notifications-to-slack' ),
+				'value' => $username,
+				'short' => true,
+			],
+			[
+				'title' => esc_html__( 'Login IP', 'dorzki-notifications-to-slack' ),
+				'value' => $_SERVER[ 'REMOTE_ADDR' ],
+				'short' => true,
+			],
+		];
+
+		return $this->slack_bot->send_message( $message, $attachments, [
+			'color' => '#27ae60',
+		] );
 
 	}
 
